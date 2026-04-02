f027fd200be77a431a139494f03c8edcc9ee43e2
wilfried229 <wilfriedhoundjetin@gmail.com>
2026-04-02 22:09:37 +0100

    update

--- kiosk.sh
+++ kiosk.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-chromium-browser \
-  --kiosk \
-  --kiosk-printing \
-  --disable-print-preview \
-  --noerrdialogs \
-  --disable-infobars \
-  --disable-session-crashed-bubble \
-  --disable-features=TranslateUI \
-  http://localhost:3000
+@echo off
+
+:loop
+taskkill /F /IM chrome.exe >nul 2>&1
+
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --kiosk --kiosk-printing --disable-print-preview --noerrdialogs --disable-infobars --disable-session-crashed-bubble --disable-features=TranslateUI --autoplay-policy=no-user-gesture-required --start-fullscreen http://localhost:3000
+
+timeout /t 2 >nul
+goto loop
