2512498b6d3473ed9edbdf647cce7e2890a99ee3
wilfried229 <wilfriedhoundjetin@gmail.com>
2026-04-02 22:25:22 +0100

    s

--- kiosk.sh
+++ kiosk.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-@echo off
-
 start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --kiosk --kiosk-printing --autoplay-policy=no-user-gesture-required --start-fullscreen http://localhost:3000
-
 exit
