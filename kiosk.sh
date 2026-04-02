52c930f0c5602ac7c9f43124498bbf203b6828f2
wilfried229 <wilfriedhoundjetin@gmail.com>
2026-04-02 22:10:21 +0100

    update

--- kiosk.sh
+++ kiosk.sh
@@ -5,7 +5,7 @@
 :loop
 taskkill /F /IM chrome.exe >nul 2>&1
 
-start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --kiosk --kiosk-printing --disable-print-preview --noerrdialogs --disable-infobars --disable-session-crashed-bubble --disable-features=TranslateUI --autoplay-policy=no-user-gesture-required --start-fullscreen http://localhost:3000
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --kiosk --kiosk-printing --autoplay-policy=no-user-gesture-required --start-fullscreen http://localhost:3000
 
 timeout /t 2 >nul
 goto loop
