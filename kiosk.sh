5baf41da3900dbe847f1392134a6137401874eb9
wilfried229 <wilfriedhoundjetin@gmail.com>
2026-04-02 22:16:23 +0100

    update

--- kiosk.sh
+++ kiosk.sh
@@ -2,10 +2,6 @@
 
 @echo off
 
-:loop
-taskkill /F /IM chrome.exe >nul 2>&1
-
 start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --kiosk --kiosk-printing --autoplay-policy=no-user-gesture-required --start-fullscreen http://localhost:3000
 
-timeout /t 2 >nul
-goto loop
+exit
